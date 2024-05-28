a9f4bd433564c7bd3711db22c806cc0345e7e1d9
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-05-28 17:34:32 +0200

    Change buch.zenathra.com to ngrok url

--- app/config/graphql.ts
+++ app/config/graphql.ts
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const GRAPHQL_ENDPOINT = isProduction
-  ? 'https://buch.zenathra.com/graphql'
+  ? 'https://alien-obliging-explicitly.ngrok-free.app/graphql'
   : 'https://localhost:3000/graphql'
