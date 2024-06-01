bdbb42744c0bee97b44ed420f342e077567f8b55
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-06-01 15:41:44 +0200

    Fix graphql endpoint url

--- app/config/graphql.ts
+++ app/config/graphql.ts
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const GRAPHQL_ENDPOINT = isProduction
-  ? 'ec2-3-75-197-52.eu-central-1.compute.amazonaws.com:3000/graphql'
+  ? 'https://ec2-3-75-197-52.eu-central-1.compute.amazonaws.com:3000/graphql'
   : 'https://localhost:3000/graphql'
