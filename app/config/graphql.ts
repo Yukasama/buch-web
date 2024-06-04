de8759f3a4a06eb21d42722e6cb75a7bf216ca30
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-06-04 12:09:14 +0200

    chore: Change aws url

--- app/config/graphql.ts
+++ app/config/graphql.ts
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 export const GRAPHQL_ENDPOINT = isProduction
-  ? 'https://ec2-18-153-49-213.eu-central-1.compute.amazonaws.com:3000/graphql'
+  ? 'https://ec2-3-71-108-167.eu-central-1.compute.amazonaws.com:3000/graphql'
   : 'https://localhost:3000/graphql'
