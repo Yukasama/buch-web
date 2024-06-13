acfb96a476ae1f187195bce43b91b3ccc132039d
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-06-13 16:10:01 +0200

    style: remove graphql functionality

--- app/config/graphql.ts
+++
@@ -1,8 +0,0 @@
-// const isProduction = process.env.NODE_ENV === 'production'
-
-export const GRAPHQL_ENDPOINT =
-  'https://ec2-35-159-81-27.eu-central-1.compute.amazonaws.com:3000/graphql'
-
-// export const GRAPHQL_ENDPOINT = isProduction
-//   ? 'https://ec2-3-71-108-167.eu-central-1.compute.amazonaws.com:3000/graphql'
-//   : 'https://localhost:3000/graphql'
--- app/config/keycloak.ts
+++
@@ -1,1 +0,0 @@
-export const GRAPHQL_ENDPOINT = 'EiP9gPXsgeJy4ekiFlHs3Y7eYkcMkbLp'
