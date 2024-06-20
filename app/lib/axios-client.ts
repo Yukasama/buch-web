43557fc0a57807c273af8ad80db5d3d6a3aff853
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-06-20 08:58:46 +0200

    Squashed commit of the following:
    commit b63b37eddde95868ab3677e7abb085c49af1e42e
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Thu Jun 20 08:46:47 2024 +0200
    
        chore: e2e runs on vercel deployment
    
    commit 3c5da9d447dce17de83d29d5f04f1cf5096ad9f8
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 15:02:09 2024 +0200
    
        chore: remove test from main to e2e
    
    commit 738e67db65db5c07540b032cd55fd5b1bbb6685f
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 14:38:25 2024 +0200
    
        chore: make server start in background
    
    commit a8a7e8ede001cbd3c874754530bb8ea40fbf53b1
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 14:24:00 2024 +0200
    
        chore: add build to test
    
    commit 83dfba10b84c55b5ae3dddcc496e00aef7e8eaeb
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 14:20:34 2024 +0200
    
        chore: install deps in test
    
    commit 5cebc0ce8eaaaf8d2599d4530cc2dc2cf4501809
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 14:17:17 2024 +0200
    
        chore: add webserver to e2e tests
    
    commit 671efa99bdcbf60cc740a68843b12e4bafc9ce36
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 07:12:54 2024 +0200
    
        chore: add pnpm setup to tests
    
    commit b71482aa14b4f3d6de29469da52b27ab0eddf5af
    Author: Yukasama <53038817+Yukasama@users.noreply.github.com>
    Date:   Wed Jun 19 07:10:08 2024 +0200
    
        chore: include tests in main.yml

--- app/lib/axios-client.ts
+++ app/lib/axios-client.ts
@@ -1,23 +1,25 @@
 import axios from 'axios'
 import dotenv from 'dotenv'
-//import https from 'node:https'
-//import path from 'node:path'
-//import fs from 'node:fs'
+import https from 'node:https'
+import path from 'node:path'
+import fs from 'node:fs'
 
-//const certPath = path.resolve('app/config/tls/certificate.crt')
-//const certificate = fs.readFileSync(certPath)
+const certPath = path.resolve('app/config/tls/certificate.crt')
+const certificate = fs.readFileSync(certPath)
 
-//const agent = new https.Agent({
-//ca: certificate,
-// rejectUnauthorized: true,
-// })
+const agent = new https.Agent({
+  ca: certificate,
+  rejectUnauthorized: true,
+})
+
+dotenv.config()
 
 /**
  * Axios client to communicate with the backend
  */
-dotenv.config()
 export const client = axios.create({
   baseURL: process.env.NEST_BACKEND_URL ?? 'http://localhost:3000',
+  httpsAgent: agent,
   headers: {
     'Content-Type': 'application/json',
   },
--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,10 +1,6 @@
 import { test, expect } from '@playwright/test'
-import dotenv from 'dotenv'
-
-dotenv.config()
-const url = process.env.REMIX_URL ?? 'http://localhost:5173'
 
 test('has title', async ({ page }) => {
-  await page.goto(url)
+  await page.goto('/')
   await expect(page).toHaveTitle('Buch-Web')
 })
