fd3c081dcda5ccab1b87d04c1ff0b16da065981a
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-06-22 21:49:49 +0200

    feat: add sonarqube analysis

---
+++ scripts/sonar-scanner.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv'
+import process from 'node:process'
+import scanner from 'sonarqube-scanner'
+
+dotenv.config()
+const sonarToken = process.env.SONAR_TOKEN
+
+scanner(
+  {
+    serverUrl: 'http://localhost:9000',
+    options: {
+      'sonar.projectName': 'buch-web',
+      'sonar.projectDescription': 'Buch Web',
+      'sonar.projectVersion': '2024.06.0',
+      'sonar.sources': 'app',
+      'sonar.tests': 'tests',
+      'sonar.token': sonarToken,
+      'sonar.scm.disabled': 'true',
+      'sonar.javascript.environments': 'node',
+      'sonar.exclusions':
+        'node_modules/**,.extras/**,.scannerwork/*,.vscode/*,coverage/**,dist/*,log/*',
+      'sonar.javascript.lcov.reportPaths': './coverage/lcov.info',
+    },
+  },
+  () => process.exit(),
+)
